af389c0abd3f84ceb1067b4c1733b1031e7b73ec
nelmas <an3744ma-s@student.lu.se>
2024-03-08 17:15:26 +0100

    First commit

--- ViewModels/OwnerDetailsViewModel.cs
+++ ViewModels/OwnerDetailsViewModel.cs
@@ -8,73 +8,74 @@ using Informatics.MauiDbClientTest.Services;
 
 namespace Informatics.MauiDbClientTest.ViewModel
 {
-public class OwnerDetailsViewModel : INotifyPropertyChanged
-{
-    private readonly IOwnerService _ownerService;
-private Owner _owner;
-public Owner Owner {
+    public class OwnerDetailsViewModel : INotifyPropertyChanged
+    {
+        private readonly IOwnerService _ownerService;
+        private Owner _owner;
+        public Owner Owner
+        {
 
-get => _owner;
-set
+            get => _owner;
+            set
 
-{
-_owner = value;
-OnPropertyChanged(nameof(Owner));
-}
-}
-public ICommand SaveOwnerCommand { get; private set; }
-public ICommand DeleteOwnerCommand { get; private set; }
+            {
+                _owner = value;
+                OnPropertyChanged(nameof(Owner));
+            }
+        }
+        public ICommand SaveOwnerCommand { get; private set; }
+        public ICommand DeleteOwnerCommand { get; private set; }
 
-public event PropertyChangedEventHandler? PropertyChanged;
-public OwnerDetailsViewModel(IOwnerService ownerService)
-{
-_ownerService = ownerService;
-SaveOwnerCommand = new Command(SaveOwner);
-DeleteOwnerCommand = new Command(DeleteOwner);
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public OwnerDetailsViewModel(IOwnerService ownerService)
+        {
+            _ownerService = ownerService;
+            SaveOwnerCommand = new Command(SaveOwner);
+            DeleteOwnerCommand = new Command(DeleteOwner);
 
-}
-
-
-private async void SaveOwner()
-{
-await _ownerService.SaveOwnerAsync(Owner);
-Shell.Current.GoToAsync("..");
-}
+        }
 
 
-private async void DeleteOwner()
-{
-await _ownerService.DeleteOwnerAsync(Owner.OwnerId);
-Shell.Current.GoToAsync("..");
-}
+        private async void SaveOwner()
+        {
+            await _ownerService.SaveOwnerAsync(Owner);
+            Shell.Current.GoToAsync("..");
+        }
 
-private void OpenOwnerDetails(string ownerId)
-{
-var route = $"{nameof(OwnerDetailsPage)}?ownerId={ownerId}";
-Shell.Current.GoToAsync(route);
-}
 
-// public async Task LoadOwnerAsync(string ownerId)
-// {
-// Owner = await _ownerService.GetOwnerAsync(ownerId);
-// }
+        private async void DeleteOwner()
+        {
+            await _ownerService.DeleteOwnerAsync(Owner.OwnerId);
+            Shell.Current.GoToAsync("..");
+        }
 
-public async Task LoadOwnerAsync(string ownerId)
-{
-Owner = await _ownerService.GetOwnerAsync(ownerId);
-if (Owner == null)
-{
-Owner = new Owner();
-Owner.OwnerId = ownerId;
-}
+        private void OpenOwnerDetails(string ownerId)
+        {
+            var route = $"{nameof(OwnerDetailsPage)}?ownerId={ownerId}";
+            Shell.Current.GoToAsync(route);
+        }
 
-}
+        // public async Task LoadOwnerAsync(string ownerId)
+        // {
+        // Owner = await _ownerService.GetOwnerAsync(ownerId);
+        // }
 
-protected virtual void OnPropertyChanged(string propertyName)
-{
-PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-}
-}
+        public async Task LoadOwnerAsync(string ownerId)
+        {
+            Owner = await _ownerService.GetOwnerAsync(ownerId);
+            if (Owner == null)
+            {
+                Owner = new Owner();
+                Owner.OwnerId = ownerId;
+            }
+
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
 
 

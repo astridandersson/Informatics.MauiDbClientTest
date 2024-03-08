d2581bba5a564c9171fbbb0a34711e2d1e71f40a
astridandersson <117929376+astridandersson@users.noreply.github.com>
2024-03-08 10:51:07 +0100

    commit

--- Services/PetService.cs
+++ Services/PetService.cs
@@ -1,6 +1,6 @@
 ﻿using Informatics.MauiDbClientTest.Contexts;
 using Informatics.MauiDbClientTest.Models;
-
+using Informatics.MauiDbClientTest.Pages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic; // Add this using directive
 using System.Threading.Tasks; // Add this using directive
@@ -19,21 +19,21 @@ public class PetService : IPetService
     }
 
 
-   public async Task<Pet> SavePetAsync(Pet pet)
-    {
+//    public async Task<Pet> SavePetAsync(Pet pet)
+//     {
     
-        // Check if the pet is a dog and its age is less than 1 year
-        if (pet.PetAge < 1)
-        {
-            // You may choose to handle this situation, e.g., throw an exception or return null
-            throw new InvalidOperationException("Dogs must be at least 1 year old.");
-        }
+//         // Check if the pet is a dog and its age is less than 1 year
+//         if (pet.PetAge < 1)
+//         {
+//             // You may choose to handle this situation, e.g., throw an exception or return null
+//             throw new InvalidOperationException("Dogs must be at least 1 year old.");
+//         }
 
-     //   If the check passes, proceed to add the pet
-        _context.Pets.Add(pet);
-        await _context.SaveChangesAsync();
-        return pet;
-    }
+//      //   If the check passes, proceed to add the pet
+//         _context.Pets.Add(pet);
+//         await _context.SaveChangesAsync();
+//         return pet;
+//     }
 
     public async Task<Pet> DeletePetAsync(string petId)
     {
@@ -64,8 +64,21 @@ public class PetService : IPetService
         return pet;
     }
     
-}
+public async Task<Pet> SavePetAsync(Pet pet)
+{
+// Set the DepartmentName property to the Department.Name value
+pet.OwnerId = pet.Owner.OwnerId;
+// Set the Department property to null to avoid PK violation
+pet.Owner = null;
+// if -1 then add new employee
 
+
+_context.Entry(pet).State = EntityState.Modified;
+
+await _context.SaveChangesAsync();
+return pet;
+}
+}
 
 
 

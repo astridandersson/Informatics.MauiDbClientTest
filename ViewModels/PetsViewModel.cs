b6e8bfef84717bb944971fbeb0df50fdfc3bf002
nelmas <an3744ma-s@student.lu.se>
2024-03-09 16:21:25 +0100

    Pets Details Page now opens

--- ViewModels/PetsViewModel.cs
+++ ViewModels/PetsViewModel.cs
@@ -7,54 +7,54 @@ using System.Collections.ObjectModel;
 
 namespace Informatics.MauiDbClientTest.ViewModel
 {
-public class PetsViewModel
-{
-private readonly IPetService _petService;
-public ObservableCollection<Pet> Pets { get; set; }
+    public class PetsViewModel
+    {
+        private readonly IPetService _petService;
+        public ObservableCollection<Pet> Pets { get; set; }
 
-public ICommand OpenAddPetCommand { get; private set; }
+        public ICommand OpenAddPetCommand { get; private set; }
 
-public ICommand OpenPetDetailsCommand { get; private set; }
+        public ICommand OpenPetDetailsCommand { get; private set; }
 
-public ICommand RefreshCommand { get; private set; }
+        public ICommand RefreshCommand { get; private set; }
 
 
-public PetsViewModel(IPetService petService)
-{
-_petService = petService;
-Pets = new ObservableCollection<Pet>();
-OpenAddPetCommand = new Command(OpenAddPet);
-OpenPetDetailsCommand = new Command<string>(OpenPetDetails);
-RefreshCommand = new Command(async () => await LoadPetsAsync());
-
-}
-private void OpenAddPet()
-{
-
-OpenPetDetails(string.Empty);
-
-}
-private void OpenPetDetails(string PetId)
-{
-var route = $"{nameof(PetsDetailsPage)}?petId={PetId}";
-Shell.Current.GoToAsync(route);
-}
-
-private async Task LoadPetsAsync()
-{
-   
-        var pets = await _petService.GetPetsAsync();
-
-        Pets.Clear();
-
-        foreach (var pet in pets)
+        public PetsViewModel(IPetService petService)
         {
-            Pets.Add(pet);
+            _petService = petService;
+            Pets = new ObservableCollection<Pet>();
+            OpenAddPetCommand = new Command(OpenAddPet);
+            OpenPetDetailsCommand = new Command<string>(OpenPetDetails);
+            RefreshCommand = new Command(async () => await LoadPetsAsync());
+
         }
-    
-}
-    
-}
+
+        private void OpenAddPet()
+        {
+            var route = $"{nameof(PetsDetailsPage)}";
+            Shell.Current.GoToAsync(route);
+        }
+        private void OpenPetDetails(string PetId)
+        {
+            var route = $"{nameof(PetsDetailsPage)}?petId={PetId}";
+            Shell.Current.GoToAsync(route);
+        }
+
+        private async Task LoadPetsAsync()
+        {
+
+            var pets = await _petService.GetPetsAsync();
+
+            Pets.Clear();
+
+            foreach (var pet in pets)
+            {
+                Pets.Add(pet);
+            }
+
+        }
+
+    }
 
 }
 
